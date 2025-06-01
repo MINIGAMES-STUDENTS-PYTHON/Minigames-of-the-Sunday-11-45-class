0b9e6da6e458d3c1bfc92b38f3851b643dc4bb80
Alessio-beep <gonzalezalessio7@gmail.com>
2025-06-01 13:20:08 +0200

    remodelando el juego

--- DonkeyKong/A1.py
+++ DonkeyKong/A1.py
@@ -2,120 +2,23 @@ import pygame
 import random
 import sys
 import os 
+from variables import ANCHO,ALTO,NEGRO,AMARILLO,BLANCO,FPS
+from modelos import Jugador,Donkey,Princesa,Plataforma,Barril
+
 
 # Incializacion
 pygame.init()
-ANCHO, ALTO = 800, 600
 VENTANA = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Donkey Kong Extendido") 
 clock = pygame.time.Clock()
-FPS = 60
-GRAVEDAD = 0.6 
 
-# Colores 
-NEGRO = (0, 0, 0)
-BLANCO = (255, 255, 255)
-AZUL = (0, 0, 255)
-ROJO = (255,0, 0)
-VERDE = (0, 255, 0)
-ROSA = (255, 182, 193)
-AMARILLO = (255, 255, 0)
-MARRON = (139, 69, 19)
+
+
 
 # Fuentes y Sonidos
 pygame.mixer.init()
 fuente = pygame.font.SysFont("Arial", 30)
 
-# Clases 
-class Jugador(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((30, 45))
-        self.image.fill(AZUL)
-        self.rect = self.image.get_rect()
-        self.rect.center = (100, ALTO - 70)
-        self.vel_y = 0 
-        self.vidas = 3
-        self.puntos = 0
-        self.en_suelo = False 
-
-    def update(self):
-        teclas = pygame.key.get_pressed()
-        if teclas[pygame.K_LEFT]:
-            self.rect.x -= 5 
-        if teclas[pygame.K_RIGHT]:
-            self.rect.x += 5
-        if teclas[pygame.K_SPACE]:
-            self.vel_y = -12
-            self.en_suelo = False 
-
-        self.vel_y += GRAVEDAD 
-        self.rect.y += self.vel_y
-
-        colisiones = pygame.sprite.spritecollide(self, plataforma, False)
-        if colisiones:
-            for plataforma in colisiones:
-                if self.vel_y > 0:
-                    self.rect.bottom = plataforma.rect.top 
-                    self.en_suelo = True 
-                    self.vel_y = 0 
-
-        if self.rect.top > ALTO:
-            self.vidas -= 1 
-            self.rect.center = (100, ALTO - 70)
-            self.vel_y = 0
-
-class Plataforma(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h):
-        super().__init__()
-        self.image = pygame.Surface((w, h))
-        self.image.fill(VERDE)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y 
-
-class Barril(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((25, 25))
-        self.image.fill(MARRON)
-        self.rect = self.image.rect_rect()
-        self.rect.x = 50 
-        self.rect.y = 60 
-        self.vel_x = random.choice([3, 4])
-        self.vel_y = 0 
-
-    def update(self):
-        self.rect.x += self.vel_x 
-        self.vel_y += GRAVEDAD
-        self.rect_y += self.vel_y 
-
-        colisiones = pygame.sprite.spritecollide(self, plataforma, False)
-        if colisiones:
-            for plataforma in colisiones:
-                if self.vel_y > 0:
-                    self.rect.bottom = plataforma.rect.top 
-                    self.vel_y = 0 
-                    self.vel_x *= -1
-
-        if self.rect.top > ALTO:
-            self.kill()
-
-class Donkey(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((60, 60))
-        self.image.fill(ROJO)
-        self.rect = self.image.get_rect()
-        self.rect.center = (100, 90)
-
-class Princesa(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(ROSA)
-        self.rect = self.image.get_rect()
-        self.rect.center = (750, 80)
 
 
 # Grupos
